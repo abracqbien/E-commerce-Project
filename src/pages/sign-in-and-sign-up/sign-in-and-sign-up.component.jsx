644b1c10a1a844891cd66b56c62a2854cafec0a0
ABracqbien <abracqbien@laposte.net>
2020-03-22 14:17:57 +0100

    Add Sign-up

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 
+// Components
+import SignIn from "../../components/sign-in/sign-in.component";
+import Signup from "../../components/sign-up/sign-up.component";
+
 // Styles
 import "./sign-in-and-sign-up.styles.scss";
-import SignIn from "../../components/sign-in/sign-in.component";
 
 const SignInAndSignUpPage = () => (
   <div className="sign-in-and-sign-up">
     <SignIn />
+    <Signup />
   </div>
 );
 
