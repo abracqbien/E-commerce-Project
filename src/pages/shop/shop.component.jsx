97ee12d31f94c64910e3f04c0fcad0caa4ba3f13
ABracqbien <abracqbien@laposte.net>
2020-07-20 22:35:45 +0200

    Advanced Router category

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,20 +1,23 @@
 import React from "react"
+import { Route } from "react-router-dom"
 import { connect } from "react-redux"
 import { createStructuredSelector } from "reselect"
 
 // Components
 import CollectionOverview from "../../components/collections-overview/collections-overview.component"
 
+// Pages
+import CollectionPage from "../collection/collection.component"
+
 // Selectors
 import { selectCollections } from "../../redux/shop/shop.selectors"
 
-const ShopPage = ({ collections }) => {
-  return (
-    <div className="shop-page">
-      <CollectionOverview />
-    </div>
-  )
-}
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+)
 
 const mapStateToProps = createStructuredSelector({
   collections: selectCollections,
