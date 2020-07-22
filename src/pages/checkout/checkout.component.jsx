918d5a6bf9e08da760a2aa77bc66d6ae4058cb83
ABracqbien <abracqbien@laposte.net>
2020-07-22 20:48:29 +0200

    Add Strip payment & favicon

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -7,6 +7,7 @@ import "./checkout.styles.scss"
 
 // Components
 import CheckoutItem from "../../components/checkout-item/checkout-item.component"
+import StripCheckoutButton from "../../components/stripe-button/stripe-button.component"
 
 // Selectors
 import {
@@ -39,6 +40,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className="total">
       <span>TOTAL: {total} €</span>
     </div>
+    <div className="test-warning">
+      *Please use the following test credit card for payments*
+      <br />
+      4242 4242 4242 4242 - Exp 01/22 - CVV: 123
+    </div>
+    <StripCheckoutButton price={total} />
   </div>
 )
 
